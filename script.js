742d708c662f99fc08fb9acfc1feeef4b2d64ba2
Ivan Fan <ivan@5andhalf.com>
2017-03-31 18:01:02 +1100

    add scriptjs

---
+++ script.js
@@ -0,0 +1,44 @@
+/**
+ * Created by yifanfan on 31/3/17.
+ */
+
+var fileContent = null;
+
+$.getJSON( "content.json", function( data ) {
+  console.log('data received ');
+  console.log(data);
+  fileContent = data;
+  $('#firstLeveTitle').html( fileContent.title);
+  $('#title').html( fileContent.content[0].title);
+  $('#thumbnail').html( fileContent.content[0].title);
+  $('#description').html( fileContent.content[0].description);
+});
+
+
+
+var currentIndex = 0;
+
+
+
+
+$( "#next" ).click(function() {
+  if (currentIndex < fileContent.content.length - 1 ){
+    currentIndex += 1;
+
+  }
+  $('#title').html( fileContent.content[currentIndex].title);
+  $('#thumbnail').html( fileContent.content[currentIndex].title);
+  $('#description').html( fileContent.content[currentIndex].description)
+
+});
+
+$( "#prev" ).click(function() {
+  if (currentIndex > 0 ){
+    currentIndex -= 1;
+  }
+  $('#title').html( fileContent.content[currentIndex].title);
+  $('#thumbnail').html( fileContent.content[currentIndex].title);
+  $('#description').html( fileContent.content[currentIndex].description)
+
+});
+
